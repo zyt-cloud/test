c3bbd541bd1621c476939a5c8ae9e3d477a9cf05
yingtao <zyt@rsung.com>
2017-05-02 09:05:30 +0800

    test

--- src/app/common/datetime.pipe.ts
+++ src/app/common/datetime.pipe.ts
@@ -10,13 +10,20 @@ export class DateTimePipe implements PipeTransform {
         
         let date = new Date(value);
 
-        let y = date.getUTCFullYear();
+        /*let y = date.getUTCFullYear();
         let M = date.getUTCMonth() + 1;
         let d = date.getUTCDate();
 
         let h = date.getUTCHours();
         let m = date.getUTCMinutes();
-        let s = date.getUTCSeconds();
+        let s = date.getUTCSeconds();*/
+        let y = date.getFullYear();
+        let M = date.getMonth() + 1;
+        let d = date.getDate();
+
+        let h = date.getHours();
+        let m = date.getMinutes();
+        let s = date.getSeconds();
 
         return y + '-' + (M < 10 ? '0' + M : M) + '-' + (d < 10 ? '0' + d : d) + ' ' + (h < 10 ? '0' + h : h) + ':' + (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s);
 
